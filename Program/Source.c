a0745db39e6a8f03fca30158987b8665fd6dd78a
Unity2033 <kileab@naver.com>
2024-11-22 19:45:59 +0900

    (2024.11.22) 포인터

--- Program/Source.c
+++ Program/Source.c
@@ -35,8 +35,72 @@ void main()
 
 #pragma region Star
 
+	//	int input = 0;
+	//	
+	//	scanf_s("%d", &input);
+	//	
+	//	for (int i = 0; i < input; i++)
+	//	{
+	//		for (int j = 0; j <= i; j++)
+	//		{
+	//			printf("★");
+	//		}
+	//	
+	//		printf("\n");
+	//	}
 
 #pragma endregion
+
+#pragma region 포인터
+	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
+
+	// int x = 10;
+	// 
+	// int * pointer = &x;
+	// 
+	// *pointer = 99;
+	// 
+	// printf("x의 변수 값 : %d\n", x);
+	// printf("x의 주소 값 : %p\n", &x);
+	// printf("pointer 변수의 값 : %p\n", pointer);
+	// printf("pointer 변수의 주소 값 : %p\n", &pointer);
+	// 
+	// // 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
+	// // 포인터 변수에 변수의 주소를 저장하게 되면 해당
+	// // 변수의 시작 주소를 가리키게 됩니다.
+	// 
+	// float health = 100.0f;
+	// 
+	// pointer = &health;
+	// 
+	// *pointer = 15.5f;
+	// 
+	// printf("health 변수의 값 : %f\n", health);
+	// 
+	// // 포인터 변수를 저장하기 위해 주소 값을 저장할 변수의
+	// // 자료형과 포인터 변수의 자료형이 일치해야 합니다.
+	// 
+	// printf("포인터 변수의 크기 : %u\n", sizeof(pointer));
+
+	// 포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수
+	// 있는 크기로 정해지며, 한 번에 처리할 수 있는 크기는 
+	// 운영 체제에 따라 크기가 결정됩니다.
+#pragma endregion
+
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을
+	// 저장할 수 있는 포인터입니다.
+
+	void * p = NULL;
+
+
+	// 범용 포인터는 메모리 주소에 접근해서 값을
+	// 변경할 수 없습니다.
+
+
+
+#pragma endregion
+
 
 
 
