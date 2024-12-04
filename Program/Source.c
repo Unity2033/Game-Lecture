bfac495e179a2fc52c7b84bc0235dc494561cc05
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-04 19:45:36 +0900

    (2024.12.4) 자기 참조 구조체

--- Program/Source.c
+++ Program/Source.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 struct GameObject
@@ -10,6 +11,18 @@ struct GameObject
 	// 구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의
 	// 크기가 다르게 설정될 수 있으며, 구조체 크기를 결정하는
 	// 형태는 기본 자료형으로만 구성되어 있습니다.
+};
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+struct Node
+{
+	int data;
+	struct Node * next;
 };
 
 int main()
@@ -48,11 +61,45 @@ int main()
 
 #pragma region 두 점 사이의 거리
 
-	printf("100의 제곱근 : %lf\n", sqrt(100));
+	//	struct Vector2 character = {0, 0};
+	//	struct Vector2 slime = { 1.75f, 1.25f };
+	//	
+	//	double x = character.x - slime.x;
+	//	double y = character.y - slime.y;
+	//	
+	//	double distance = sqrt(pow(x, 2) + pow(y, 2));
+	//	
+	//	if (distance >= 3.0)
+	//	{
+	//		printf("이동 상태\n");
+	//	}
+	//	else if (distance < 3.0)
+	//	{
+	//		printf("공격 상태\n");
+	//	}
+	//	
+#pragma endregion
 
-	printf("10² : %lf\n", pow(10, 2));
+#pragma region 자기 참조 구조체
+	// 구조체 내부에 자기 자신의 자료형을 멤버 변수로 
+	// 가지고 있는 구조체입니다.
+
+	struct Node * node1 = malloc(sizeof(struct Node));
+	struct Node * node2 = malloc(sizeof(struct Node));
+	struct Node * node3 = malloc(sizeof(struct Node));
+
+	node1->data = 10;
+	node2->data = 20;
+	node3->data = 30;
+
+	node1->next = &node2;
+	node2->next = &node3;
+	node3->next = NULL;
+
+
 
 #pragma endregion
+
 
 
 	return 0;
