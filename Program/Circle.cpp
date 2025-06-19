a59fa26d563c3446a3c149bcc55a051005cd5299
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-06-19 21:44:15 +0900

    (2025.6.19) 다형성

---
+++ Program/Circle.cpp
@@ -0,0 +1,16 @@
+#include "Circle.h"
+
+Circle::Circle()
+{
+	cout << "Created Circle" << endl;
+}
+
+void Circle::Volume()
+{
+	cout << "¥ð x r©÷\n" << endl;
+}
+
+Circle::~Circle()
+{
+	cout << "Release Circle" << endl;
+}
---
+++ Program/Circle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Shape.h"
+
+class Circle : public Shape
+{
+public:
+	Circle();
+
+	void Volume();
+
+	~Circle();
+
+};
+
