1fdf634dea1396950b226443490bedb183649812
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-01 21:44:29 +0900

    (2025.7.1) 선형 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,8 +1,49 @@
 ﻿#include <iostream>
+#include <vector>
+#include <list>
 
 using namespace std;
 
 int main()
 {
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
+
+#pragma region vector container
+
+	// std::vector<int> vector;
+	// 
+	// vector.reserve(8);
+	// 
+	// vector.push_back(10);
+	// vector.push_back(20);
+	// vector.push_back(30);
+	// vector.push_back(40);
+	// vector.push_back(50);
+	// 
+	// vector.pop_back();
+	// 
+	// for (int i = 0; i < vector.size(); i++)
+	// {
+	// 	cout << vector[i] << endl;
+	// }
+	// 
+	// vector.clear();
+	// 
+	// cout << "vector의 Size 값 : " << vector.size() << endl;
+	// cout << "vector의 Capacity 값 : " << vector.capacity() << endl;
+
+#pragma endregion
+
+#pragma region list container
+
+
+#pragma endregion
+
+
+
+#pragma endregion
+
+
 	return 0;
 }
