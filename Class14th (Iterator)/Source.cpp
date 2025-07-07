b078401becc574b2a20a4ad4b1687a5ba922ff50
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-07 21:40:32 +0900

    (2025.7.7) 객체 지향 프로그래밍

---
+++ Class14th (Iterator)/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>	
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+#pragma region 반복자
+
+	//	std::vector<int> vector;
+	//	
+	//	vector.push_back(10);
+	//	vector.push_back(20);
+	//	vector.push_back(30);
+	//	vector.push_back(40);
+	//	vector.push_back(50);
+	//	
+	//	for (std::vector<int>::iterator iterator = vector.begin(); iterator != vector.end(); iterator++)
+	//	{
+	//		cout << *iterator << endl;
+	//	}
+
+#pragma endregion
+
+	return 0;
+}
