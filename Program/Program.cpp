fb95aa0432cb4d7231e76d0d4971ac09bc894ebc
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-10-16 19:40:47 +0900

    (2025.10.16) 이중 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -10,70 +10,26 @@ private:
     {
         T data;
         Node * next;
+        Node * previous;
     };
 
     int size;
-
     Node * head;
-
+    Node * tail;
 public:
     List()
     {
         size = 0;
         head = nullptr;
+        tail = nullptr;
     }
 
-    void push_front(T data)
-    {
-        Node * newNode = new Node;
-
-        newNode->data = data;
-
-        if (head == nullptr)
-        {
-            head = newNode;
-
-            newNode->next = nullptr;
-        }
-        else
-        {
-            newNode->next = head;
-
-            head = newNode;
-        }
-
-        size++;
-    }
-
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "linked list is empty" << endl;
-        }
-        else
-        {
-            Node * deleteNode = head;
-
-            head = deleteNode->next;
-
-            delete deleteNode;
-
-            size--;
-        }
-    }
 
 };
 
 int main()
 {
     List<int> list;
-
-    list.push_front(10);
-    list.push_front(5);
-
-    list.pop_front();
-    list.pop_front();
 
     return 0;
 }
