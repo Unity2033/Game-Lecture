117fcae23f51dcfc3cb659156daba17c8eb607ef
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-05-20 19:44:13 +0900

    (2026.5.20) 다형성

---
+++ Program/Publisher.cpp
@@ -0,0 +1,17 @@
+#include "Publisher.h"
+#include <iostream>
+
+void Publisher::Send(int frequency)
+{
+	std::cout << "frequency : " << frequency << std::endl;
+}
+
+void Publisher::Send(const char* message)
+{
+	std::cout << "Message : " << message << std::endl;
+}
+
+void Publisher::Send(const char* message, int frequency)
+{
+	std::cout << "Message : " << message << frequency <<  std::endl;
+}
