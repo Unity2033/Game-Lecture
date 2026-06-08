2c1508d5cea8d968ac79e2ae227e653fa322f1e4
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-06-08 19:47:14 +0900

    (2026.6.8) 반복자

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,37 +1,6 @@
 ﻿#include "stdafx.h"
-#include <set>
 
 int main()
 {
-#pragma region 연관 컨테이너
-    // 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
-
-#pragma region set
-
-    // std::set<const char*> set;
-    // 
-    // set.insert("League of Legend");
-    // set.insert("Fortnite");
-    // set.insert("Minecraft");
-    // set.insert("Valorant");
-    // 
-    // for (const auto & element : set)
-    // {
-    //     cout << element << endl;
-    // }
-    // 
-    // cout << "set empty : " << set.empty() << endl;
-    // 
-    // set.clear();
-    // 
-    // cout << "set size : " << set.size() << endl;
-    // cout << "set max size : " << set.max_size() << endl;
-
-#pragma endregion
-
-
-#pragma endregion
-
-
     return 0;
 }
